0be59ec3a81a1741903ed0451c04b1a90801d961
mohammadzm0101 <zm441572@gmail.com>
2021-11-29 10:14:12 -0800

    end

--- js/single.js
+++ js/single.js
@@ -1,7 +1,28 @@
 // for display in html page in browser
-// document.getElementById('#').innerHTML = 5 + 6;
+// document.getElementById('js-test').innerHTML= 2+7;
 
 // for html but test
-// document.write(5 + 6);
+// document.write(5 + 5); inside html tag like botton 
+// windows.alert(5+6);
+var x = "mohammad";
+var z = x.length;
+var text = "mohammad plz go"
+z = text.indexOf("go");
+z = text.slice(5 , 8);
+z = text.slice(-9 , 3);
+z = text.substring(5 , 8);
+z = text.substr(5 , 4);
+z = text.replace('javascript' , 'js');
+
+// text
+z = text.toLocaleLowerCase();
+
+z = text.concat(x);
+z = text.trim()
+z = numbers.split(',')
+console.log(z)
+
+
+
 
 
