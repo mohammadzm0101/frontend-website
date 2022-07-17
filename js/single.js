35a0c7517a87eacfa42eecaeaa30739466057b52
mohammadzm0101 <zm441572@gmail.com>
2022-07-17 18:55:43 +0430

    ok

--- js/single.js
+++ js/single.js
@@ -1,1 +1,15 @@
-
+// 
+let num = [0, 1, 4, 9]
+let num1 = num.at(3)
+console.log(num1)
+let testNumber = 3
+comparison()
+function comparison() {
+    let result;
+    if (testNumber > num1) {
+        result = 'true'
+    } else {
+        result = 'false'
+    }
+    console.log(result)
+}
